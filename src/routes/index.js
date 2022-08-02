e9316338cef3a0cd0fd8c9b948d20111289afac9
Luiz Abbott <luiz_abbott@epam.com>
2022-08-02 16:21:41 -0300

    feat: adding views to listings

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
+import { addViewToListingRoute } from "./addViewToListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListings";
 
 export default [
   getAllListingsRoute,
-  getListingRoute
+  getListingRoute,
+  addViewToListingRoute
 ]
