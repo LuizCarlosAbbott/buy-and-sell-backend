c190649bfef435801c7b6ffb47edb6d76a34bdee
Luiz Abbott <luiz_abbott@epam.com>
2022-08-02 16:25:43 -0300

    feat: Get User Listings

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { addViewToListingRoute } from "./addViewToListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListings";
+import { getUserListingsRoute } from "./getUserListings";
 
 export default [
   getAllListingsRoute,
   getListingRoute,
-  addViewToListingRoute
+  addViewToListingRoute,
+  getUserListingsRoute
 ]
