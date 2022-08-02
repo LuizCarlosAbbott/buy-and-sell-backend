4e0ccf7d0f909cd37fcacc55868604f8def1ea2a
Luiz Abbott <luiz_abbott@epam.com>
2022-08-02 17:04:00 -0300

    feat: update listing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import { createNewListingRoute } from "./createNewListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListings";
 import { getUserListingsRoute } from "./getUserListings";
+import { updatedListingRoute } from "./updateListing";
 
 export default [
   getAllListingsRoute,
   getListingRoute,
   addViewToListingRoute,
   getUserListingsRoute,
-  createNewListingRoute
+  createNewListingRoute,
+  updatedListingRoute
 ]
